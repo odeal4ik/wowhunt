94d2d2f73fd8b136c321ab8bc0c562aa1ef2549f
odeal4ik <odeal4ik@mail.ru>
2024-10-25 13:53:32 +0200

    added 1024 stylesfor header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import { Header } from '@/components/header/header';
 import { HeroBlock } from '@/components/hero-block/hero-block';
-import { Splitter } from '@/components/splitter/splitter';
-import { GamesPlates } from '@/components/games-plates/games-plates';
-import { GamesTabs } from '@/components/games-tabs/games-tabs';
-import { Support } from '@/components/support/support';
-import { WhyUs } from '@/components/why-us/why-us';
-import { HowItWorks } from '@/components/how-it-works/how-it-works';
-import { Carousel } from '@/components/carousel/carousel';
+// import { Splitter } from '@/components/splitter/splitter';
+// import { GamesPlates } from '@/components/games-plates/games-plates';
+// import { GamesTabs } from '@/components/games-tabs/games-tabs';
+// import { Support } from '@/components/support/support';
+// import { WhyUs } from '@/components/why-us/why-us';
+// import { HowItWorks } from '@/components/how-it-works/how-it-works';
+// import { Carousel } from '@/components/carousel/carousel';
 import { Footer } from '@/components/footer/footer';
 
 export default function Home() {
@@ -17,7 +17,7 @@ export default function Home() {
             <main>
                 <HeroBlock />
 
-                <Splitter title="CHOOSE GAME" />
+                {/* <Splitter title="CHOOSE GAME" />
 
                 <GamesPlates />
 
@@ -37,7 +37,7 @@ export default function Home() {
 
                 <Splitter title="24/7 ONLINE SUPPORT" />
 
-                <Support />
+                <Support /> */}
             </main>
 
             <Footer />
