eae301dd6981269538c6c17cb69624bb8b3ecca2
Nikita Kudenikov <nikita.kudenikov@travelgsm.com>
2025-03-14 13:26:01 +0100

    added rewrites to config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,18 @@ const nextConfig: NextConfig = {
             },
         },
     },
+
+    images: {
+        domains: [process.env.NEXT_PUBLIC_IMAGE_DOMAIN as string],
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/storage/:path*',
+                destination: `https://${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}/storage/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
